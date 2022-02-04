065ad74720f819c6827d0df5690fa9662f3a3196
Naoki Murano <naokimurano@outlook.com>
2022-02-05 06:54:23 +0900

    update pool

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x896eDE222D3f7f3414e136a2791BDB08AAa25Ce0',
-  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x9b989a7b8963f4b08ec094710e2966fb3c7f6c43'
+  cakeAddress: '0x7D572502Bd99f8830F32F2F4CF0fC8767282A992',
+  busdAddress: '0x04068da6c83afcfa0e13ba15a6696662335d5b75',
+  lpAddress: '0x7d4b803319c500b785cb7e5987c0bd95924f3df4'
 }
 
 export default priceContracts
