8d2780ccdef5bcd50bd0dc78bfa1467294591f17
Naoki Murano <naokimurano@outlook.com>
2022-03-03 02:24:22 +0900

    update lp address

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xf393E9Ea75befAb72D5063dE4c1b380955E7C560',
   busdAddress: '0x04068da6c83afcfa0e13ba15a6696662335d5b75',
-  lpAddress: '0x610031a2896ea3bf62079c5fcb401ae034112b5a'
+  lpAddress: '0x4c8d8e64ac5225c4b4ce753dab67464c07353208'
 }
 
 export default priceContracts
